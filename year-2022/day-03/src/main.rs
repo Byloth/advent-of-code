a6d38be013f2b6e1982ede495f393c07f7832f5a
Matteo Bilotta <me@byloth.net>
2022-12-07 16:27:49 +0100

    [IMP] Third day solved! (Part two)

--- year-2022/day-03/src/main.rs
+++ year-2022/day-03/src/main.rs
@@ -12,37 +12,48 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     #[test]
     fn test_solution() {
         let backpacks = parse_content(TEST_INPUT);
+        let compartments = split_into_compartments(&backpacks);
 
-        let result = sum_priorities(&backpacks);
-        assert_eq!(result, 157);
+        let duplicates_sum = sum_duplicates_priority(&compartments);
+        assert_eq!(duplicates_sum, 157);
+
+        let groups = split_into_groups(&backpacks);
+        let badges_sum = sum_badges_priority(&groups);
+        assert_eq!(badges_sum, 70);
     }
 }
 
 const GROUP_SIZE: usize = 3;
 
-fn parse_content(content: &str) -> Vec<(&str, &str)> {
+fn parse_content(content: &str) -> Vec<&str> {
     let mut backpacks = vec![];
 
     for items in content.split("\n") {
-        let index = items.len() / 2;
-
-        let left = &items[..index];
-        let right = &items[index..];
-
-        backpacks.push((left, right));
+        backpacks.push(items);
     }
 
     return backpacks;
 }
 
-fn split_into_groups(backpacks: &Vec<(&str, &str)>) -> Vec<&[&(&str, &str)]> {
-    let length = backpacks.len();
+fn split_into_compartments<'a>(backpacks: &'a Vec<&str>) -> Vec<(&'a str, &'a str)> {
+    let mut compartments = vec![];
+
+    for items in backpacks {
+        let index = items.len() / 2;
+
+        let left = &items[..index];
+        let right = &items[index..];
+
+        compartments.push((left, right));
+    }
+
+    return compartments;
+}
+fn split_into_groups<'a>(backpacks: &'a Vec<&str>) -> Vec<Vec<&'a str>> {
     let mut groups = vec![];
 
-    for index in (0..length).step_by(GROUP_SIZE) {
-        let group = &backpacks[index..index + GROUP_SIZE];
-    
-        groups.push(group);
+    for chunk in backpacks.chunks(GROUP_SIZE) {
+        groups.push(chunk.to_vec());
     }
 
     return groups;
@@ -57,6 +68,29 @@ fn find_duplicate_item(backpack: (&str, &str)) -> Option<char> {
     
     return None;
 }
+fn find_group_badge(group: &Vec<&str>) -> Option<char> {
+    fn _recursive_search(_group: &[&str], _look_for: char) -> Option<char> {
+        let _current = _group[0];
+
+        if _group[0].contains(_look_for) {
+            if _group.len() == 1 {
+                return Some(_look_for);
+            } else {
+                return _recursive_search(&_group[1..], _look_for);
+            }
+        }
+
+        return None;
+    }
+
+    for character in group[0].chars() {
+        if let Some(result) = _recursive_search(&group[1..], character) {
+            return Some(result);
+        }
+    }
+
+    return None;
+}
 
 fn compute_priority(item: char) -> i32 {
     let ascii_value = item as i32;
@@ -67,11 +101,23 @@ fn compute_priority(item: char) -> i32 {
 
     return ascii_value - 38;
 }
-fn sum_priorities(backpacks: &Vec<(&str, &str)>) -> i32 {
+
+fn sum_duplicates_priority(backpacks: &Vec<(&str, &str)>) -> i32 {
     let mut sum = 0;
 
-    for backpack in backpacks {
-        if let Some(item) = find_duplicate_item(*backpack) {
+    for compartments in backpacks {
+        if let Some(item) = find_duplicate_item(*compartments) {
+            sum += compute_priority(item);
+        }
+    }
+
+    return sum;
+}
+fn sum_badges_priority(groups: &Vec<Vec<&str>>) -> i32 {
+    let mut sum = 0;
+
+    for group in groups {
+        if let Some(item) = find_group_badge(group) {
             sum += compute_priority(item);
         }
     }
@@ -83,7 +129,13 @@ fn main() {
     let content = include_str!("input.txt");
 
     let backpacks = parse_content(content);
-    let priorities_sum = sum_priorities(&backpacks);
+    let compartments = split_into_compartments(&backpacks);
+    let duplicates_sum = sum_duplicates_priority(&compartments);
 
-    println!("Sum of priorities: {}", priorities_sum);
+    println!("Sum of duplicates priority: {}", duplicates_sum);
+
+    let groups = split_into_groups(&backpacks);
+    let badges_sum = sum_badges_priority(&groups);
+
+    println!("Sum of badges priority: {}", badges_sum);
 }
