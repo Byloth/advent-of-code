187449bcf22e94bc7b10a732c3d1595b9a412f03
Matteo Bilotta <me@byloth.net>
2022-12-14 16:12:06 +0100

    [FIX] Now it uses `struct`s. 😌

--- year-2022/day-04/src/main.rs
+++ year-2022/day-04/src/main.rs
@@ -12,83 +12,85 @@ mod tests {
     #[test]
     fn test_solution() {
         let schedules = parse_content(TEST_INPUT);
-        let redundant_schedules = count_redundant_schedules(&schedules, is_schedule_redundant);
+        let redundant_schedules = schedules.iter()
+                                                  .filter(|pair| pair.is_redundant())
+                                                  .count();
 
         assert_eq!(redundant_schedules, 2);
 
-        let overlapping_schedules = count_redundant_schedules(&schedules, is_schedule_overlapping);
+        let overlapping_schedules = schedules.iter()
+                                                    .filter(|pair| pair.is_overlapping())
+                                                    .count();
 
         assert_eq!(overlapping_schedules, 4);
     }
 }
 
-type Schedule = (i32, i32);
-type SchedulePair = (Schedule, Schedule);
+#[derive(Debug)]
+struct Schedule {
+    start: i32,
+    end: i32
+}
+impl Schedule {
+    fn from_str(value: &str) -> Self {
+        let (start, end) = value.split_once('-').unwrap();
+
+        return Self {
+            start: start.parse().unwrap(),
+            end: end.parse().unwrap()
+        };
+    }
+
+    fn contains(&self, other: &Schedule) -> bool {
+        return self.start <= other.start && other.end <= self.end;
+    }
+    fn overlaps(&self, other: &Schedule) -> bool {
+        return self.start <= other.start && other.start <= self.end;
+    }
+}
+
+#[derive(Debug)]
+struct SchedulePair {
+    left: Schedule,
+    right: Schedule
+}
+impl SchedulePair {
+    fn from_str(value: &str) -> Self {
+        let pairs = value.split_once(',').unwrap();
+
+        return Self {
+            left: Schedule::from_str(pairs.0),
+            right: Schedule::from_str(pairs.1)
+        };
+    }
+
+    fn is_overlapping(&self) -> bool {
+        return self.left.overlaps(&self.right) || self.right.overlaps(&self.left);
+    }
+    fn is_redundant(&self) -> bool {
+        return self.left.contains(&self.right) || self.right.contains(&self.left);
+    }
+}
 
 fn parse_content(content: &str) -> Vec<SchedulePair> {
-    let mut result: Vec<SchedulePair> = vec![];
-
-    for line in content.lines() {
-        let pairs = line.split_once(',').unwrap();
-
-        let left_pair = pairs.0.split_once('-').unwrap();
-        let right_pair = pairs.1.split_once('-').unwrap();
-
-        result.push((
-            (left_pair.0.parse().unwrap(), left_pair.1.parse().unwrap()),
-            (right_pair.0.parse().unwrap(), right_pair.1.parse().unwrap())
-        ));
-    }
-
-    return result;
-}
-
-fn is_schedule_redundant(schedule: &SchedulePair) -> bool {
-    let (left, right) = schedule;
-
-    if left.0 <= right.0 && left.1 >= right.1 {
-        return true;
-    }
-    if right.0 <= left.0 && right.1 >= left.1 {
-        return true;
-    }
-
-    return false;
-}
-fn is_schedule_overlapping(schedule: &SchedulePair) -> bool {
-    let (left, right) = schedule;
-
-    if left.0 <= right.0 && left.1 >= right.0 {
-        return true;
-    }
-    if right.0 <= left.0 && right.1 >= left.0 {
-        return true;
-    }
-
-    return false;
-}
-
-fn count_redundant_schedules(schedules: &Vec<SchedulePair>, check_fn: fn(&SchedulePair) -> bool) -> i32 {
-    let mut result = 0;
-
-    for schedule in schedules {
-        if check_fn(schedule) {
-            result += 1;
-        }
-    }
-
-    return result;
+    return content.lines()
+                  .map(SchedulePair::from_str)
+                  .collect();
 }
 
 fn main() {
     let content = include_str!("input.txt");
 
     let schedules = parse_content(content);
-    let redundant_schedules = count_redundant_schedules(&schedules, is_schedule_redundant);
+    let redundant_schedules = schedules.iter()
+                                              .filter(|pair| pair.is_redundant())
+                                              .count();
 
     println!("Redundant schedules: {}", redundant_schedules);
 
-    let overlapping_schedules = count_redundant_schedules(&schedules, is_schedule_overlapping);
+    let overlapping_schedules = schedules.iter()
+                                                .filter(|pair| pair.is_overlapping())
+                                                .count();
 
     println!("Overlapping schedules: {}", overlapping_schedules);
 }
