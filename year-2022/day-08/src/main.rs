ab9d4cf75e9d9bad4330209883d4292ecb61a798
Matteo Bilotta <me@byloth.net>
2023-01-03 11:27:27 +0100

    [FIX] Intense refactoring for the eighth day.

--- year-2022/day-08/src/main.rs
+++ year-2022/day-08/src/main.rs
@@ -29,33 +29,25 @@ fn parse_content(content: &str) -> Vec<Vec<u32>> {
                   .collect::<Vec<Vec<u32>>>();
 }
 
-fn get_max_x_tree_height(forest: &[Vec<u32>], x: usize, start: usize, end: usize) -> u32 {
-    let mut max_height = 0;
-
-    for y in start..end {
-        if forest[y][x] > max_height {
-            max_height = forest[y][x];
-        }
-    }
-
-    return max_height;
+fn collect_row(forest: &[Vec<u32>], x: usize) -> Vec<u32> {
+    return forest.iter()
+                 .map(move |row| row[x])
+                 .collect();
 }
-fn get_max_y_tree_height(forest: &[Vec<u32>], y: usize, start: usize, end: usize) -> u32 {
-    let mut max_height = 0;
+fn range_iter(start: usize, end: usize) -> impl Iterator<Item = usize> {
+    let range: Box<dyn Iterator<Item = usize>> = if start < end {
+        Box::new(start + 1..end)
+    } else {
+        Box::new((end..start).rev())
+    };
 
-    for x in start..end {
-        if forest[y][x] > max_height {
-            max_height = forest[y][x];
-        }
-    }
-
-    return max_height;
+    return range;
 }
 
-fn is_tree_visible(forest: &[Vec<u32>], x: usize, y: usize) -> bool {
-    let width = forest[0].len();
-    let length = forest.len();
-
+fn get_forest_size(forest: &[Vec<u32>]) -> (usize, usize) {
+    return (forest[0].len(), forest.len());
+}
+fn is_edge_tree(x: usize, y: usize, width: usize, length: usize) -> bool {
     if x == 0 || x == (width - 1) {
         return true;
     }
@@ -63,32 +55,66 @@ fn is_tree_visible(forest: &[Vec<u32>], x: usize, y: usize) -> bool {
         return true;
     }
 
-    let tree_height = forest[y][x];
+    return false;
+}
 
-    if tree_height > get_max_x_tree_height(forest, x, 0, y) {
+fn get_max_tree_height(trees: &[u32], start: usize, end: usize) -> u32 {
+    let mut max_height = 0;
+
+    for index in range_iter(start, end) {
+        let height = trees[index];
+
+        if height > max_height {
+            max_height = height;
+        }
+    }
+
+    return max_height;
+}
+fn get_view_distance(trees: &[u32], start: usize, end: usize) -> u32 {
+    let tree_height = trees[start];
+
+    let mut view_distance = 0;
+    for index in range_iter(start, end) {
+        view_distance += 1;
+
+        if tree_height <= trees[index] {
+            break;
+        }
+    }
+
+    return view_distance;
+}
+
+fn is_tree_visible(forest: &[Vec<u32>], x: usize, y: usize, width: usize, length: usize) -> bool {
+    if is_edge_tree(x, y, width, length) {
         return true;
     }
-    if tree_height > get_max_x_tree_height(forest, x, y + 1, length) {
+
+    let tree_height = forest[y][x];
+    if tree_height > get_max_tree_height(&collect_row(forest, x), y, 0) {
         return true;
     }
-    if tree_height > get_max_y_tree_height(forest, y, 0, x) {
+    if tree_height > get_max_tree_height(&collect_row(forest, x), y, length) {
         return true;
     }
-    if tree_height > get_max_y_tree_height(forest, y, x + 1, width) {
+    if tree_height > get_max_tree_height(&forest[y], x, 0) {
+        return true;
+    }
+    if tree_height > get_max_tree_height(&forest[y], x, width) {
         return true;
     }
 
     return false;
 }
 fn count_visible_trees(forest: &[Vec<u32>]) -> usize {
-    let width = forest[0].len();
-    let length = forest.len();
+    let (width, length) = get_forest_size(forest);
 
     let mut count = 0;
 
     for y in 0..length {
         for x in 0..width {
-            if is_tree_visible(forest, x, y) {
+            if is_tree_visible(forest, x, y, width, length) {
                 count += 1;
             }
         }
@@ -97,78 +123,29 @@ fn count_visible_trees(forest: &[Vec<u32>]) -> usize {
     return count;
 }
 
-fn get_x_view_distance(forest: &[Vec<u32>], y: usize, start: usize, end: usize) -> u32 {
-    let tree_height = forest[y][start];
-
-    let range;
-    if start < end {
-        range = ((start + 1)..end).collect::<Vec<usize>>();
-    }
-    else {
-        range = (end..start).rev().collect::<Vec<usize>>();
+fn compute_scenic_score(forest: &[Vec<u32>], x: usize, y: usize, width: usize, length: usize) -> u32 {
+    if is_edge_tree(x, y, width, length) {
+        return 0;
     }
 
-    let mut view_distance = 0;
-    for x in range {
-        view_distance += 1;
-
-        if tree_height <= forest[y][x] {
-            break;
-        }
-    }
-
-    return view_distance;
-}
-fn get_y_view_distance(forest: &[Vec<u32>], x: usize, start: usize, end: usize) -> u32 {
-    let tree_height = forest[start][x];
-
-    let range;
-    if start < end {
-        range = ((start + 1)..end).collect::<Vec<usize>>();
-    }
-    else {
-        range = (end..start).rev().collect::<Vec<usize>>();
-    }
-
-    let mut view_distance = 0;
-    for y in range {
-        view_distance += 1;
-
-        if tree_height <= forest[y][x] {
-            break;
-        }
-    }
-
-    return view_distance;
-}
-
-fn compute_scenic_score(forest: &[Vec<u32>], x: usize, y: usize) -> u32 {
     let width = forest[0].len();
     let length = forest.len();
 
-    if x == 0 || x == (width - 1) {
-        return 0;
-    }
-    if y == 0 || y == (length - 1) {
-        return 0;
-    }
-
-    let top_view_distance = get_x_view_distance(forest, y, x, 0);
-    let bottom_view_distance = get_x_view_distance(forest, y, x, width);
-    let left_view_distance = get_y_view_distance(forest, x, y, 0);
-    let right_view_distance = get_y_view_distance(forest, x, y, length);
+    let top_view_distance = get_view_distance(&collect_row(forest, x), y, 0);
+    let bottom_view_distance = get_view_distance(&collect_row(forest, x), y, length);
+    let left_view_distance = get_view_distance(&forest[y], x, 0);
+    let right_view_distance = get_view_distance(&forest[y], x, width);
 
     return top_view_distance * bottom_view_distance * left_view_distance * right_view_distance;
 }
 fn get_highest_scenic_score(forest: &[Vec<u32>]) -> u32 {
-    let width = forest[0].len();
-    let length = forest.len();
+    let (width, length) = get_forest_size(forest);
 
     let mut highest_score = 0;
 
     for y in 0..length {
         for x in 0..width {
-            let score = compute_scenic_score(forest, x, y);
+            let score = compute_scenic_score(forest, x, y, width, length);
 
             if score > highest_score {
                 highest_score = score;
